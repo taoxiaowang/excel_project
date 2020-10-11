d303e3bfb5f0a2e7b52d2b6c1e12cac11836308c
taoxiaowang <15828028350@163.com>
2020-10-11 16:14:38 +0800

    feat:初始化化工程

---
+++ app/src/main/java/com/friendscircle/main/TestActivity.java
@@ -0,0 +1,25 @@
+package com.friendscircle.main;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.friendscircle.R;
+
+/**
+ * author : taowang
+ * date :2020/10/11
+ * description:
+ **/
+public class TestActivity extends AppCompatActivity {
+
+    public class testActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.app_test);
+
+        }
+    }
+
+
+}
